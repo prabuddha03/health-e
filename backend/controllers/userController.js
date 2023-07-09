295104d371b90c94a757a283557352721feb9bde
Prabuddha Chowdhury <prabuddha.chowdhury@gmail.com>
2023-07-09 16:28:12 +0530

    Update userController.js

---
+++ backend/controllers/userController.js
@@ -0,0 +1,12 @@
+const User = require('./../models/userModel');
+const catchAsync= require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+const factory = require('./handlerFactory');
+
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach(el => {
+      if (allowedFields.includes(el)) newObj[el] = obj[el];
+    });
+    return newObj;
+  };
